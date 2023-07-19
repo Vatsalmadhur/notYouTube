07ed603114b77c49f2c6743a4cbe5aafeb317c08
Madhur <bhartimadhur21@gmail.com>
2023-07-19 21:31:51 +0530

    Channel Stuff done finally

--- yt/src/Components/ChannelDetail.jsx
+++ yt/src/Components/ChannelDetail.jsx
@@ -1,25 +1,35 @@
-import React, {useEffect, useState} from 'react'
-import {useParams} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import fetchFromApi from "./utils/fetchFromApi";
+import { Box } from "@mui/material";
+import { lightBlue } from "@mui/material/colors";
+import ChannelCard from "./ChannelCard";
+import Videos from './Videos'
 
 const ChannelDetail = () => {
-  const [channelDetail,setChannelDetail]=useState(null)
-  const [videos,setVideos]=useState([])
-  const {id}=useParams();
-  console.log(channelDetail,videos)
-  useEffect(()=>{
-    fetchFromApi(`channels?.part=snippet&id=${id}&maxResult=50`)
-        .then((data)=>
-         setChannelDetail(data?.data?.items[0]))
-// console.log(data))
-    fetchFromApi(`search?channelId=${id}&part=snippet&order=date&maxResults=50`)
-        .then((data)=>
-            setVideos(data?.data?.items))
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+  const { id } = useParams();
+  console.log(channelDetail, videos);
+  useEffect(() => {
+    fetchFromApi(`channels?.part=snippet&id=${id}&maxResult=50`).then((data) =>
+      setChannelDetail(data?.data?.items[0])
+    );
+    // console.log(data))
+    fetchFromApi(
+      `search?channelId=${id}&part=snippet&order=date&maxResults=50`
+    ).then((data) => setVideos(data?.data?.items));
     // console.log(id)
-  },[id])
+  }, [id]);
   return (
-      <div>{id}</div>
-  )
-}
+    <Box sx={{ minHeight: "95vh" }}>
+      <Box
+        sx={{ backgroundColor: "#ffffff", height: "300px", zIndex: 10 }}
+      ></Box>
+      <ChannelCard channelDetail={channelDetail}/>
+      <Videos videos={videos}/>
+    </Box>
+  );
+};
 
-export default ChannelDetail
+export default ChannelDetail;
